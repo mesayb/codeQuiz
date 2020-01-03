719a74e7cac949c008e401e50ac94844ff9ebec6
Mesay Bekele <me2say@gmail.com>
2020-01-03 18:19:34 -0500

    fix eventListener on choices li

--- js/dynamic.js
+++ js/dynamic.js
@@ -13,132 +13,132 @@ var welcome_container = document.querySelector(".welcome_container");
 highScoreButton.classList.add("btn", "btn-primary");
 
 
-function createWelcome(){
-//create welcome page
-//header
-var welcomeRow = document.createElement("div");
-welcome_container.appendChild(welcomeRow);
-welcomeRow.classList.add("row");
-var welcomeCol = document.createElement("div");
-welcome_container.appendChild(welcomeCol);
-welcomeCol.classList.add("col-sm-12")
-var welcomeH3 = document.createElement("h3");
-welcomeH3.classList.add("text-center","mb-4", "mt-4");
-welcomeH3.textContent="Coding Quiz Challenge";
-welcome_container.appendChild(welcomeH3);
+function createWelcome() {
+    //create welcome page
+    //header
+    var welcomeRow = document.createElement("div");
+    welcome_container.appendChild(welcomeRow);
+    welcomeRow.classList.add("row");
+    var welcomeCol = document.createElement("div");
+    welcome_container.appendChild(welcomeCol);
+    welcomeCol.classList.add("col-sm-12")
+    var welcomeH3 = document.createElement("h3");
+    welcomeH3.classList.add("text-center", "mb-4", "mt-4");
+    welcomeH3.textContent = "Coding Quiz Challenge";
+    welcome_container.appendChild(welcomeH3);
 
-//description
-var welcomeRow2 = document.createElement("div");
-welcomeRow2.classList.add("row");
-var welcomeCol2 = document.createElement("div");
-welcomeCol2.classList.add("col-sm-12")
-var welcomePar = document.createElement("p");
-welcomePar.classList.add("text-center");
-welcomePar.textContent="Try to answer the following code-related questions within the time limit. Keep in mind that incorrect anwers will penalize your scoretime by 10sec";
-welcome_container.appendChild(welcomeRow2);
-welcomeRow2.appendChild(welcomeCol2);
-welcomeCol2.appendChild(welcomePar);
+    //description
+    var welcomeRow2 = document.createElement("div");
+    welcomeRow2.classList.add("row");
+    var welcomeCol2 = document.createElement("div");
+    welcomeCol2.classList.add("col-sm-12")
+    var welcomePar = document.createElement("p");
+    welcomePar.classList.add("text-center");
+    welcomePar.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect anwers will penalize your scoretime by 10sec";
+    welcome_container.appendChild(welcomeRow2);
+    welcomeRow2.appendChild(welcomeCol2);
+    welcomeCol2.appendChild(welcomePar);
 
-//button
-var welcomeRow3 = document.createElement("div");
-welcomeRow3.classList.add("row");
-var welcomeCol3 = document.createElement("div");
-welcomeCol3.classList.add("col-sm-12", "text-center")
- startQuizButton = document.createElement("button");
-startQuizButton.classList.add("btn", "btn-primary", "mb-4", "mt-2");
-startQuizButton.textContent="Start Quiz";
-welcome_container.appendChild(welcomeRow3);
-welcomeRow3.appendChild(welcomeCol3);
-welcomeCol3.appendChild(startQuizButton);
+    //button
+    var welcomeRow3 = document.createElement("div");
+    welcomeRow3.classList.add("row");
+    var welcomeCol3 = document.createElement("div");
+    welcomeCol3.classList.add("col-sm-12", "text-center")
+    startQuizButton = document.createElement("button");
+    startQuizButton.classList.add("btn", "btn-primary", "mb-4", "mt-2");
+    startQuizButton.textContent = "Start Quiz";
+    welcome_container.appendChild(welcomeRow3);
+    welcomeRow3.appendChild(welcomeCol3);
+    welcomeCol3.appendChild(startQuizButton);
 
 };
 
 createWelcome();
 //add eventlistner
 
-startQuizButton.addEventListener("click", function(){
+startQuizButton.addEventListener("click", function () {
+
     content[0].remove();
     questionNumber();
+
 })
 
 
 
-//create a page to request number of questions
-function questionNumber(){
-    var numberOfQuestions;
+var numberOfQuestions;
 
+//create a page to request number of questions
+function questionNumber() {
     question_number.classList.add("text-center", "mt-3");
-  //  var questionDiv = document.createElement("div");
+
     var questionPrompt = document.createElement("h5");
     var input = document.createElement("input");
     var buttonContainer = document.createElement("div");
     var submitButton = document.createElement("button");
 
-questionPrompt.textContent = "To start taking a quiz please enter number of questions : ";
-question_number.appendChild(questionPrompt);
+    questionPrompt.textContent = "To start taking a quiz please enter number of questions : ";
+    question_number.appendChild(questionPrompt);
 
 
-questionPrompt.classList.add("mt-3");
-input.classList.add("mt-3");
+    questionPrompt.classList.add("mt-3");
+    input.classList.add("mt-3");
 
-input.setAttribute("placeHolder","number of questions");
-question_number.appendChild(input);
+    input.setAttribute("placeHolder", "number of questions");
+    question_number.appendChild(input);
 
-submitButton.textContent = "Enter";
-buttonContainer.classList.add("text-center");
-submitButton.classList.add("btn","btn-primary","m-3");
-buttonContainer.appendChild(submitButton);
-question_number.appendChild(buttonContainer);
+    submitButton.textContent = "Enter";
+    buttonContainer.classList.add("text-center");
+    submitButton.classList.add("btn", "btn-primary", "m-3");
+    buttonContainer.appendChild(submitButton);
+    question_number.appendChild(buttonContainer);
 
 
-submitButton.addEventListener("click", function(event){
-    event.preventDefault;
-    numberOfQuestions = input.value;
-    console.log("numberOfQuestions = " + numberOfQuestions);
-    // questionPage(numberOfQuestions);
-    content[1].remove();
-    questionPage(1);
-})
-//return numberOfQuestions;
+    submitButton.addEventListener("click", function (event) {
+        event.preventDefault;
+        numberOfQuestions = parseInt(input.value);
+        content[1].remove();
+        questionPage(numberOfQuestions);
+    })
 }
 
 
 
 //create a page to display the questions
 function questionPage(index) {
-    
-
-  //  content[1].remove();
     var questionDiv = document.createElement("div");
+    questionDiv.setAttribute("class", "custom_qstn_container");
     var questionDesc = document.createElement("p");
-    questionDesc.textContent = `${index+1}. ${questions[index].title}`;
+    var questionOL = document.createElement("ol");
+    questionOL.setAttribute("class", "custom_ol");
+    questionOL.setAttribute("id", "custom-ol-id");
+
+    if(index > 0){
+    questionDesc.textContent = `${index}. ${questions[index].title}`;
+    }
     questionDiv.appendChild(questionDesc);
     questionDiv.classList.add("mt-4");
-    var questionOL = document.createElement("ol");
-    // questionOL.setAttribute("type","a");
+
+    //  questionOL.setAttribute("type","a");
     questionDiv.appendChild(questionOL);
-    
-    for(var i=0; i < questions[index].choices.length && index > 0; i++){
+
+    for (var i = 0; i < questions[index].choices.length && index > 0; i++) {
         var questionLI = document.createElement("li");
         questionLI.textContent = questions[index].choices[i];
         questionLI.classList.add("btn", "btn-light", "d-flex", "mb-3");
         questionOL.appendChild(questionLI);
     }
-  //  questionNumber.remove();
+
+    //append ol to the question container div
     question_container.appendChild(questionDiv);
 
-    // questionOL.addEventListener("click", function(event){
-    //   //  event.preventDefault;
-    //     if((event.target.nodeName) === "LI"){
-    //         index--;
-    //         i = 0;
-    //         console.log("li clicked");
-    //     }
-       
-    // })
-}  
+    //capture the div 
+    var custom_qstn_container = document.querySelector(".custom_qstn_container");
 
-
-
-
- 
+    custom_qstn_container.addEventListener("click", function (event) {
+        if ((event.target.nodeName) === "LI") {
+            question_container.removeChild(custom_qstn_container);
+            numberOfQuestions = numberOfQuestions - 1;
+            questionPage(numberOfQuestions);
+        }
+    });
+}
--- js/questions.js
+++ js/questions.js
@@ -10,9 +10,29 @@ var questions = [
       answer: "parentheses"
     },
     {
-      title: "fuiituyftrtrt665655555555555555555555555555555555555:",
-      choices: ["stringsrpojtowpqjt[oiqerwjtpojwerotjpowerjtrewt", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      title: "Select the character that ends every statement",
+      choices: [".", ";", "!", "]"],
+      answer: ";"
     },
+    {
+      title: "Select the illegal character in this statement. var P2_a%88$;",
+      choices: ["%", "P", "_", "$"],
+      answer: "%"
+    },
+    {
+      title: "===, !==, >, and <= are ________ operators.",
+      choices: ["assignment", "random", "parentheses", "comparison"],
+      answer: "comparison"
+    },
+    {
+      title: "Which of the following is true about variable naming conventions in JavaScript?",
+      choices: ["JavaScript variable names must begin with a letter or the underscore character.", "JavaScript variable names are case sensitive.", "Both of the above.", "None of the above."],
+      answer: "parentheses"
+    },
+    {
+      title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+      choices: ["last()", "put()", "push()", "all"],
+      answer: "push()"
+    }
   ];
 
